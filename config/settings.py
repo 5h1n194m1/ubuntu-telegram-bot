2bacdf9eb38fc18fe0352382c1fb7d045c67ab60
zul <aufamuradana@gmail.com>
2026-02-24 11:04:13 +0700

    update

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,4 @@
 # config/settings.py
-# Semua konfigurasi & path dipusatkan di sini
 
 import os
 from pathlib import Path
@@ -12,8 +11,11 @@ BASE_DIR = Path("/home/zul/zulbot")
 TOKEN = os.getenv("TOKEN")
 
 # User config
-ALLOWED_IDS = {1135391914, 871102197, 8036828631}
-OWNER_ID = {1135391914, 871102197}
+raw_ids = os.getenv("ALLOWED_IDS","")
+ALLOWED_IDS = {int(x.strip()) for x in raw_ids.split(",")} if raw_ids else set()
+
+# Owner ID keperluan notif khusus
+OWNER_ID =INT(OS.GETENV("OWNER_ID", 1135391914))
 
 # Storage paths
 STORAGE_DIR = BASE_DIR / "storage"
@@ -28,3 +30,9 @@ DOWNLOAD_DIR.mkdir(parents=True, exist_ok=True)
 
 INFO_FILE.touch(exist_ok=True)
 LOG_FILE.touch(exist_ok=True)
+
+# memastikan file log & info selalu ada
+if not INFO_FILE.exists():
+    INFO_FILE.touch()
+if not LOG_FILE.exists():
+    LOG_FILE.touch()
